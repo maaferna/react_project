2d10d57a91289fc81abf97f3acc277648eccf171
mafparra <mafernandezparra@gmail.com>
2022-08-30 22:24:41 -0400

    Updated home page, used Card component in Home component.

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import DishDetail from "./DishDetailComponent";
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import Contact from './ContactComponent';
+
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS
     };
   }
 
@@ -19,8 +27,9 @@ class Main extends Component {
 
     const HomePage = () => {
       return(
-          <Home 
-          />
+          <Home  dish={this.state.dishes.filter((dish)=> dish.featured)[0]}
+                promotion={this.state.promotions.filter((promo)=> promo.featured)[0]} 
+                leader={this.state.leaders.filter((leader)=> leader.featured)[0]}/>
       );
     }
 
@@ -29,7 +38,8 @@ class Main extends Component {
         <Header/>
         <Routes>
           <Route path="/home" element={ <HomePage/> } />
-          <Route path="/menu" render={(props) => {<Menu data={this.state.dishes} />}} />
+          <Route path='/contactus' component={ < Contact />} />
+          <Route path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route path="*" element={<Navigate replace to="/home" />} />
         </Routes>
         <Footer/>
