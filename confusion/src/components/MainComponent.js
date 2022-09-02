711228a6079b209212b00bbb821820c11dea3795
mafparra <mafernandezparra@gmail.com>
2022-09-01 21:42:30 -0400

    Repair page Menu, was added breadcrum option and Configure the routes in your React router configuration to enable the use of route parameters within the URL to pass information to a component.

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -33,14 +33,23 @@ class Main extends Component {
       );
     }
 
+    const DishWithId = ({match}) => {
+      return (
+        <DishDetail dish={this.state.dishes.filter((dish)=>dish.id===parseInt(match.params.dishId,10))[0]}
+         comments={this.state.comments.filter((comment)=>comment.dishId === parseInt(match.params.dishId,10))}
+         />
+      );
+    }
+
     return (
       <div>
         <Header/>
         <Routes>
-          <Route path="/home" element={ <HomePage/> } />
+          <Route path='/home' element={ <HomePage/> } />
           <Route path='/contactus' element={ < Contact />} />
-          <Route path="/menu" render={() => <Menu dishes={this.state.dishes} />} />
-          <Route path="*" element={<Navigate replace to="/home" />} />
+          <Route exact path='/menu' element={<Menu dishes={this.state.dishes} />} />
+          <Route path='/menu/:dishId' element={DishWithId} />
+          <Route path='*' element={<Navigate replace to="/home" />} />
         </Routes>
         <Footer/>
       </div>
