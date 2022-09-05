e062314ceecfaed1334b0ceb4a1b1b72c6ec1d61
mafparra <mafernandezparra@gmail.com>
2022-09-05 17:39:55 -0300

    new updated to navbar

--- confusion/src/components/HeaderComponent.js
+++ confusion/src/components/HeaderComponent.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse,
+     NavItem, Button, Modal, ModalHeader, ModalBody, 
+     Form, FormGroup, Label, Input } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
     constructor(props) {
         super(props);
-        this.toggleNav = this.toggleNav.bind(this);
         this.state = {
-          isNavOpen: false
+          isNavOpen: false,
+          isModalOpen: false
         };
+        this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
       }
     
       toggleNav() {
@@ -16,39 +21,66 @@ class Header extends Component {
           isNavOpen: !this.state.isNavOpen
         });
       }
+
+      toggleModal() {
+          this.setState({
+            isModalOpen: !this.state.isModalOpen
+          });
+      }
+
+      handleLogin(event) {
+        this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value +
+        " Remember: " + this.remember.checked);
+        event.preventDefault();
+      }
      
     render() {
         return (
             <React.Fragment>
+                <div>
                 <Navbar className="navbar navbar-dark navbar-expand-sm fixed-top">
-                    <div className="container-fluid">
+                    <div className="container">
                         <NavbarToggler onClick={this.toggleNav}/>
-                        <NavbarBrand className="mr-auto" href="/"><img src="assets/images/logo.png" height="30" width="41"
-                        alt="Ristorante Con Fusion"/></NavbarBrand>                        
-                        <Collapse isOpen={this.state.isNavOpen} navbar>
-                            <Nav navbar>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/home">
-                                        <span className="fa fa-home fa-lg"></span>Home
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/aboutus">
-                                        <span className="fa fa-info fa-lg"></span>About Us
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/menu">
-                                        <span className="fa fa-list fa-lg"></span>Menu
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/contactus">
-                                        <span className="fa fa-address-card fa-lg"></span>Contact
-                                    </NavLink>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
+                        <div className="row">
+                            <div className="col-10">
+                                <NavbarBrand className="mr-auto" href="/"><img src="assets/images/logo.png" height="30" width="41"
+                                alt="Ristorante Con Fusion"/></NavbarBrand>                        
+                                <Collapse isOpen={this.state.isNavOpen} navbar>
+                                    <Nav navbar>
+                                        <NavItem>
+                                            <NavLink className="nav-link" to="/home">
+                                                <span className="fa fa-home fa-lg"></span>Home
+                                            </NavLink>
+                                        </NavItem>
+                                        <NavItem>
+                                            <NavLink className="nav-link" to="/aboutus">
+                                                <span className="fa fa-info fa-lg"></span>About Us
+                                            </NavLink>
+                                        </NavItem>
+                                        <NavItem>
+                                            <NavLink className="nav-link" to="/menu">
+                                                <span className="fa fa-list fa-lg"></span>Menu
+                                            </NavLink>
+                                        </NavItem>
+                                        <NavItem>
+                                            <NavLink className="nav-link" to="/contactus">
+                                                <span className="fa fa-address-card fa-lg"></span>Contact
+                                            </NavLink>
+                                        </NavItem>
+                                    </Nav>
+                                </Collapse>
+                            </div>
+                            <div className="col">
+                                <Nav className="ml-auto" navbar>
+                                    <NavItem>
+                                        <Button outline onClick={this.toggleModal}>
+                                            <span className="fa fa-sign-in fa-lg">Login</span>
+                                        </Button>
+                                    </NavItem>
+                                </Nav>
+                            </div>
+                        </div>
                     </div>
                 </Navbar>
 
@@ -64,7 +96,31 @@ class Header extends Component {
                         </div>
                     </div>
                 </div>
-
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                innerRef={(input) =>this.username = input}></Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                innerRef={(input) =>this.password = input}></Input>
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                    innerRef={(input) =>this.remember = input}>Remember Me</Input>
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+                </div>
             </React.Fragment>
         )
     }
